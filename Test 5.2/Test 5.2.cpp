9421dbde09fede264147bbe67ee9a775ebb8bd6c
BGBro <bohdan.horbachov.itis.2020@lpnu.ua>
2020-11-08 14:00:05 +0200

    master

---
+++ Test 5.2/Test 5.2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../5.2/5.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Test52
+{
+	TEST_CLASS(Test52)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = A(3., 2, 2.);
+			Assert::AreEqual(t, 9);
+		}
+	};
+}
